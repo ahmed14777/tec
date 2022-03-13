4d5e2f25cb81a5799c6fbdbe1e8a678aced9ea48
ahmed14777 <ah9621777@gmail.com>
2022-03-13 09:50:35 +0100

    modified js file

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector("#menu");
 const list = document.querySelector("#links");
+const mediaQuery = window.matchMedia("(max-width: 767px)");
 list.style.transition = "1s";
+if (mediaQuery.matches) {
+    list.style.visibility = "hidden";
+    list.style.width = "0";
+    list.style.height = "0";
+    list.style.opacity = "0";
+}
 menu.onclick = function () {
     if (list.style.visibility != "hidden") {
         list.style.visibility = "hidden";
